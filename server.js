9b6394777c18f4dcb99d13d75b4164a73977da2c
Rosa Turner <rmturner1220@gmail.com>
2022-08-24 22:40:46 -0400

    updated routes

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ const config = {
 };
 const hbs = exphbs.create(config);
 
+const app = express();
+const PORT = process.env.PORT || 3005
+
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -30,27 +33,24 @@ const sess = {
     })
 };
 
-const app = express();
-const PORT = process.env.PORT || 3005
+app.use(session(sess));
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Router
+app.use(routes);
+
 //required for Express Handlebars
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-
-app.use(session(sess));
 
 app.use(function (req, res, next) {
     res.locals.session = req.session;
     next();
 });
-
-// Router
-app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
